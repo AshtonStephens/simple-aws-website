20de9919a84003df2d651c2c6546729c95f24a86
AshtonStephens <ashton.cs.stephens@gmail.com>
2023-06-06 11:03:32 -0400

    Add website

---
+++ website/test/index.test.ts
@@ -0,0 +1,20 @@
+import * as index from "../src/index";
+
+describe("TestButtonCalls", () => {
+
+  beforeEach(() => {
+    // To do before each test, set up mock api.
+  });
+
+  it("should get the message count", async () => {
+    // TODO: Write tests
+  });
+
+  it("should create a message", async () => {
+    // TODO: Write tests
+  });
+
+  it("should get a message", async () => {
+    // TODO: Write tests
+  });
+});
